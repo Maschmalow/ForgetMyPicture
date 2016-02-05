859249cc8391b1c3bec11ec3dea7f65e97c5e1c7
Maschmalow <maschmalow@gmail.com>
2016-02-05 17:13:39 +0100

    debug

--- WebSearch/search/search/spiders/search_spider.py
+++ WebSearch/search/search/spiders/search_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+import logging
+from search.items import SearchItem
 from itertools import combinations
 from urlparse import urlparse
 from urlparse import parse_qs
@@ -10,17 +12,22 @@ class SearchSpider(scrapy.Spider):
         "http://www.google.fr/search?tbm=isch&q="
     ]
     
-    def __init__(self, keywords, *arg, **kwargs):
+    
+    def __init__(self, keywords, *args, **kwargs):
         super(SearchSpider, self).__init__(*args, **kwargs)
         all_keywords = keywords.split(" ")
-        self.name = all_keywords[0].append(all_keywords[1])
-        self.keywords = tails(all_keywords, 2)
+        self.name = all_keywords[:2]
+        self.keywords = all_keywords[2:]
+        self.done = False
         
     def start_requests(self):
         requests = []
-        for nb_keywords in range(len(keywords)):
+        for nb_keywords in range(len(self.keywords)):
             for cur_keywords in combinations(self.keywords, nb_keywords):
-                requests.append(scrapy.http.Request(start_urls|0] + ''.join(cur_keywords)))
+                logging.info("request URL: " + self.start_urls[0]  +  ''.join(self.name) + ''.join(cur_keywords))
+                if not self.done:
+                    requests.append(scrapy.http.Request(self.start_urls[0]  +  ''.join(self.name) + ''.join(cur_keywords)))
+                    self.done = True
                 
         return requests
             
@@ -28,9 +35,9 @@ class SearchSpider(scrapy.Spider):
     def parse(self, response):
         for result in response.xpath('//div[@data-ri and @data-row]/a'):
             item = SearchItem()
-            link_url_query = parse_qs(urlparse(result.xpath('@href').extract()).query)
+            link_url_query = parse_qs(urlparse(result.xpath('@href').extract()[0]).query)
             
-            item['img_url'] = link_url_query.get('imgurl')
-            item['site_url'] = link_url_query.get('imgrefurl')
+            item['img_url'] = link_url_query.get('imgurl')[0]
+            item['site_url'] = link_url_query.get('imgrefurl')[0]
             yield item
         
