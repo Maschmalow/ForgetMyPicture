2e681b1e218dec1b063f001a6bda7b7c5f6ca292
Antoine Durand <maschmalow@gmail.com>
2016-02-21 23:03:49 +0100

    User data storage and setup various improvements

--- AndroidApp/app/src/main/java/net/tenwame/forgetmypicture/SearchActivity.java
+++ AndroidApp/app/src/main/java/net/tenwame/forgetmypicture/SearchActivity.java
@@ -3,8 +3,12 @@ package net.tenwame.forgetmypicture;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
 
@@ -18,14 +22,25 @@ public class SearchActivity extends Activity {
         setContentView(R.layout.activity_search);
 
         keywordsField = (EditText) findViewById(R.id.keywords_field);
+        keywordsField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    startSearchFromUI(v);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void startSearchFromUI(View view) {
         Log.v(TAG, "Search started from UI");
         String[] keywords = keywordsField.getText().toString().split(" ");
-        Searcher.getInstance().startSearch(keywords);
+        Searcher.startSearch(keywords);
 
-
+        Toast.makeText(this, R.string.search_started_toast, Toast.LENGTH_SHORT).show();
+        finish();
     }
 
 
