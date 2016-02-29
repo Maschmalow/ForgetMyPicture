ee3b75bcab61f727d0fb6460c2f4f85b43a0cd79
pplumier <pplumier@enseirb-matmeca.fr>
2016-02-29 12:25:43 +0100

    envoi de mails avec SMTP

---
+++ envoi_mail_webmaster/envoi_mail.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE_ADR 256
+#define NB_ADR 10
+
+int main(int argc, char ** argv)
+{
+  char *path = argv[1]; //le nom de l'exemple est a passer en argument
+
+  FILE *fichier = fopen(path, "r");
+  char line[SIZE_ADR + 20];
+  char adr[NB_ADR][SIZE_ADR];
+  int i, j;
+  fgets(line, SIZE_ADR*sizeof(char), fichier);
+
+  for (i = 0;
+       line[0] != '@' && line[1] != '!' && line[2] != '@' && line[3] != '!' && line[4] != '@' && line[5] != '!'
+	 && line[6] != 'F' && line[7] != 'I' && line[8] != 'N'
+	 && line[9] != '@' && line[10] != '!' && line[11] != '@' && line[12] != '!' && line[13] != '@' && line[14] != '!';
+       i++)
+    {
+      if (i < NB_ADR)
+	{
+	  sscanf(line, "e-mail:\t%[^\n]\n", adr[i]);
+	  fgets(line, SIZE_ADR*sizeof(char), fichier);
+	}
+      else
+	fprintf(stderr, "tableau d'adresse trop petit\n");
+    }
+
+  fclose(fichier);
+  
+  for (j = 0; j < i && j < NB_ADR; j++)
+    {
+      system("telnet smtp.enseirb-matmeca.fr 25");
+      system("HELO enseirb.fr");
+      system("MAIL FROM:FMP@enseirb-matmeca.fr");
+      char * chaine;
+      asprintf(&chaine, "RCPT TO:%s", adr[j]);
+      system(chaine);
+      free(chaine);
+      system("DATA");
+      system("Dear ..."); //ici on mettra le contenu/texte du mail
+      system("");
+      system(".");
+      system("QUIT");    
+    }
+ 
+  return 0; 
+}
