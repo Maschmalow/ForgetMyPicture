e4cd474e14560a9fb4c52c8535f82f90b13d04a7
Antoine Durand <maschmalow@gmail.com>
2016-03-27 00:46:00 +0100

    get_info

--- server_scripts/get_info.php
+++ server_scripts/get_info.php
@@ -10,12 +10,16 @@ if($user->deviceId == '0')  {
 
 $ret = array()
 foreach( $request as R::find('request', 'user_id = ?', [$user->deviceId])) {
+    $ret[$request->id] = array()
     foreach($result as R::find('result', 'request_id = ?', [$request->id]) {
-        if($result->match != -1)
-            
+        if($result->match != -1) {
+            $ret[$request->id][$result->picURL] = $result->match;
+            R::trash($result)
+        }
     }
 }
 
+echo json_encode($ret, JSON_FORCE_OBJECT);
 
 
 R::close();
