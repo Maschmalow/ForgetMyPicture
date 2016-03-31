d39b8592b26041f66dedf98749330edd7038d1d3
pplumier <pplumier@enseirb-matmeca.fr>
2016-03-31 17:49:02 +0200

    amélioration

--- envoi_mail_webmaster/who_is.c
+++ envoi_mail_webmaster/who_is.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define SIZE_MAX 256 // Maximum size of a web site domain
+
 /**
  * \fn int main(int argc, char **argv)
  * \brief who_is program start.
@@ -19,12 +21,49 @@
  */
 int main(int argc, char ** argv)
 {
+  int i;
+  int found = 0;
+  char * domain_name = "";
+  char tmp[SIZE_MAX];
+
+  for (i = 0; i < strlen(argv[1]) && !found; i++)
+    {
+      strcpy(tmp, domain_name);
+
+      if (i)
+	free(domain_name);
+
+      if (argv[1][i] != '.')
+	asprintf(&domain_name, "%s%c", tmp, argv[1][i]);
+      else
+	{
+	  if (argv[1][i+1] == 'f' &&  argv[1][i+2] == 'r')
+	    {
+	      asprintf(&domain_name, "%s.fr", tmp);
+	      found = 1;
+	    }
+	  else
+	    {
+	      if (argv[1][i+1] == 'n' &&  argv[1][i+2] == 'e' &&  argv[1][i+3] == 't')
+		{
+		  asprintf(&domain_name, "%s.net", tmp);
+		  found = 1;
+		}
+	      else
+	        asprintf(&domain_name, "");
+	    }
+	}
+    }
+
+  // Do the function whois with the web site domain
   char * chaine;
-  asprintf(&chaine, "whois %s | grep e-mail", argv[1]);
-  system(chaine);
-  asprintf(&chaine, "whois %s | grep Admin | grep Email", argv[1]);
+  asprintf(&chaine, "whois %s | grep e-mail", domain_name);
   system(chaine);
   free(chaine);
+  asprintf(&chaine, "whois %s | grep Admin | grep Email", domain_name);
+  system(chaine);
+  free(chaine);
+  free(domain_name);
   printf("@!@!@!FIN@!@!@!\n");
   return EXIT_SUCCESS; 
 }
